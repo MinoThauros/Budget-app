6bc00b10f7b272496b26ccdc523ecda6073a2cb1
Herimino Rakotondrafara <heriminorakotondrafara@Heriminos-MacBook-Pro.local>
2022-09-04 19:41:19 -0400

    deleted redux

--- redux/store.tsx
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import ExpenseReducer from '../redux/expenses';
-
-export const store=configureStore({
-    reducer:{
-        ExpenseReducer:ExpenseReducer
-    }
-})
