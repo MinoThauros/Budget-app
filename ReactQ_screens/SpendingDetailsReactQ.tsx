d68f91b13cd88c47c9385543340e0d233e27d13c
MinoThauros <herimino@timatech.ca>
2023-05-24 07:49:26 -0400

    created reactQ component for deletion

--- ReactQ_screens/SpendingDetailsReactQ.tsx
+++ ReactQ_screens/SpendingDetailsReactQ.tsx
@@ -1,18 +1,60 @@
 import { View, Text,Button, StyleSheet} from 'react-native'
 import { spending } from '../models/spending';
 import { useQueryClient } from '@tanstack/react-query';
+import { useDeleteExpense } from '../Hooks/ReactQ';
+import Spending from '../components/Spending';
 
 const SpendingDetailsReactQ = ({navigation,route}:any) => {
+    const {spending}=route.params
 
+    const onSuccess=({data}:{data:spending[]})=>{
+        console.log(data)
+        navigation.goBack()
+
+    }
     const queryClient = useQueryClient();
+
+    const {mutate,error,isSuccess}=useDeleteExpense({onSuccess,queryClient});
+
+
+    const deleteSpending= ()=>{
+        return mutate(spending.id)
+    }
     
 
-    return(
+    
+    const Content=()=>{
+        var Details:JSX.Element=(
         <View>
-            <Text>Spending detail using React Query</Text>
+            <Spending spending={spending} Delete={deleteSpending} Edit={()=>{}}/>
         </View>
+            
+        )
+        if (isSuccess){
+            Details=(
+                <View style={{flex:1}}>
+                    <Text>Deleted Spending</Text>
+                </View>
+            )
+        }
+        if (error){
+            Details=(
+                <View style={{flex:1}}>
+                    <Text>Something wrong happened; try again later</Text>
+                </View>
+            )
+        }
+        
+        return Details
+    }
+
+    return (
+        <View >
+            <Content/>
+        </View>
+        
     )
-};
+}
 
 
 export default SpendingDetailsReactQ
